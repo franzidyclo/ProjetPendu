b0643798b83399a52175228c2ad727e7381b899e
franzidyclo <ptestmonmail@gmail.com>
2022-06-02 15:46:38 +0200

    Version_one

---
+++ PlayPendu.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        Projet Creation Pendu
+# Purpose:
+#
+# Author:      Fandyclo
+#
+# Created:     17/05/2022
+# Copyright:   (c) Fandyclo 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+""" Creation Projet Jeu le Pendu """
+
+
+""" creer le Dictionnaire """
+
+
+
+dictionnaire_voiture = [("Renault","Megane"), ("Renault","Twingo"), ("Renault","") ]
+capitales = dict (dictionnaire_voiture)
+print(dictionnaire_voiture)
+
+
+
+
+
+
+
